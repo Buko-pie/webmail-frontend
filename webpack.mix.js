e477d0114e90b640b5d9d993cd8110f2d0ac4dbc
Aleksey Ratnikov <alex@arttour.com.ua>
2021-07-09 21:47:27 +0300

    add ide-helper

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,18 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.disableNotifications();
+
+if(!mix.inProduction()){
+  mix.webpackConfig({ devtool: "inline-source-map" });
+}
+mix.copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/', 'public/webfonts');
+
+mix.options({
+  processCssUrls: false,
+});
+
 mix.js('resources/js/app.js', 'public/js')
   .vue()
   .postCss('resources/css/app.css', 'public/css', [
@@ -19,4 +31,4 @@ mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/syncfusion_material.scss', 'public/css')
   .sass('resources/sass/sidebarTemplate.scss', 'public/css')
   .sass('resources/sass/inboxTemplate.scss', 'public/css');
-  
+
