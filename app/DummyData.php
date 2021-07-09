f4ac7ba1eb1cfa05395321b797886c513baca087
Aleksey Ratnikov <alex@arttour.com.ua>
2021-07-09 21:31:05 +0300

    add ide-helper

--- app/DummyData.php
+++ app/DummyData.php
@@ -5,6 +5,26 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
+/**
+ * App\DummyData
+ *
+ * @property int $id
+ * @property string $sender
+ * @property string $receiver
+ * @property string|null $message
+ * @property int $starred
+ * @property int $important
+ * @property int $read
+ * @property mixed|null $labels
+ * @property int $has_attachment
+ * @property string|null $attachment_link
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|DummyData newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|DummyData newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|DummyData query()
+ * @mixin \Eloquent
+ */
 class DummyData extends Model
 {
   protected $connection = "mysql";
--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Users
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Users newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Users newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Users query()
+ * @mixin \Eloquent
+ */
 class Users extends Model
 {
   protected $connection = "mysql";
