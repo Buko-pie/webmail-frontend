66c81f3f58a1ec5e9253a3a5056197e635f78010
Aleksey Ratnikov <alex@arttour.com.ua>
2021-07-09 21:31:46 +0300

    add ide-helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider {
     /**
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-
+        if(App::isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
