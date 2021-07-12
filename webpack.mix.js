6c926d2d0fd0ad57220bcb42d74dd455d5c3284b
Aleksey Ratnikov <alex@arttour.com.ua>
2021-07-12 18:45:58 +0300

    improve styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-if(!mix.inProduction()){
-  mix.webpackConfig({ devtool: "inline-source-map" });
+if (!mix.inProduction()) {
+  mix.webpackConfig({devtool: "inline-source-map"});
 }
 mix.copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/', 'public/fonts');
 mix.copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/', 'public/webfonts');
@@ -24,10 +24,10 @@ mix.options({
 });
 
 mix.js('resources/js/app.js', 'public/js')
-  .vue()
-  .postCss('resources/css/app.css', 'public/css', [
-    require("tailwindcss"),
-  ])
-  .sass('resources/sass/app.scss', 'public/css')
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [
+      require("tailwindcss"),
+    ])
+    .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps(false)
 
