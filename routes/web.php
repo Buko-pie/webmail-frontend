eef822b3e84cc8e5e0a9569a7b4111f1a0b03eab
Salvador Jr. Amba <salvadorjr.amba@gmail.com>
2021-07-22 14:59:42 +0800

    email view fix

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebmailController;
-use App\Http\Controllers\DummyDataController;
+use App\Http\Controllers\DataController;
 use App\Http\Controllers\AccountsController;
 
 /*
