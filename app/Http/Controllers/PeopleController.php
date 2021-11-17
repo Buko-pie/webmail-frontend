b0e5101f6566beae8f6abe084442400882e268a3
Salvador Jr. Amba <salvadorjr.amba@gmail.com>
2021-11-17 17:57:06 +0800

    added PeopleController

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Dacastro4\LaravelGmail\Facade\LaravelGmail;
+
+class PeopleController extends Controller
+{
+  public function list(Request $request)
+  {
+    $pageSize = $request->pageSize ?? 50;
+    $pageToken = $request->pageToken ?? '';
+    $requestSyncToken = $request->requestSyncToken ?? false;
+    $syncToken = $request->syncToken ?? '';
+
+    $optParams = array(
+      'pageSize'          => $pageSize,
+      'pageToken'         => $pageToken,
+      'requestSyncToken'  => $requestSyncToken,
+      'syncToken'         => $syncToken,
+      'readMask'          => 'names,emailAddresses,phoneNumbers,metadata',
+    );
+    return LaravelGmail::people()->list($optParams);
+  }
+
+  public function search(Request $request)
+  {
+    $pageSize = $request->pageSize ?? 50;
+    $optParams = array(
+      'query'    => $request->query,
+      'pageSize' => $pageSize,
+      'readMask' => 'names,emailAddresses,phoneNumbers,metadata',
+    );
+
+    return LaravelGmail::people()->search($optParams);
+  }
+}
